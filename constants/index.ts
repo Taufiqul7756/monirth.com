41bd1292c2dc8463023d98ab0c417f14961f2c50
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-02-27 01:30:46 +0600

    NavBar Route - default

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "HOME" },
+  { href: "/", key: "shop", label: "SHOP" },
+  { href: "/", key: "about", label: "ABOUT" },
+  { href: "/", key: "contact_us", label: "CONTACT" },
+];
