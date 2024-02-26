a22960f2b4dbdce880ca517c660f1a76c943710c
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-02-26 20:59:09 +0600

    Navbar added

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import HeroPage from "./pages/HeroSection/page";
+import ProductsPage from "./pages/Products/Products";
+
+export default function Home() {
+  return (
+    <main>
+      <>
+        <HeroPage />
+        <ProductsPage />
+      </>
+    </main>
+  );
+}
