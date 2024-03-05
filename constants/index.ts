3c13d7da5a9f564303994fa9b8355259ed688413
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-06 01:05:18 +0600

    products added

--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,54 @@ export const NAV_LINKS = [
   { href: "/", key: "about", label: "ABOUT" },
   { href: "/", key: "contact_us", label: "CONTACT" },
 ];
+
+export const affiliateProducts = [
+  {
+    id: 1,
+    name: "Product 1",
+    imageUrl: "https://ibb.co/Y3gtzY1",
+    affiliateLink:
+      "https://www.daraz.com.bd/products/30m-i329074822-s1702818159.html?spm=a2a0e.home.just4u.1.573712f70RJfqT&scm=1007.28811.376629.0&pvid=1fe404fe-28ff-4cb5-b1d6-ee635c27bce1&clickTrackInfo=pvid%3A1fe404fe-28ff-4cb5-b1d6-ee635c27bce1%3Bchannel_id%3A0000%3Bmt%3Ahot%3Bitem_id%3A329074822%3B",
+  },
+  {
+    id: 1,
+    name: "Product 2",
+    imageUrl: "https://ibb.co/Y3gtzY1",
+    affiliateLink:
+      "https://www.daraz.com.bd/products/30m-i329074822-s1702818159.html?spm=a2a0e.home.just4u.1.573712f70RJfqT&scm=1007.28811.376629.0&pvid=1fe404fe-28ff-4cb5-b1d6-ee635c27bce1&clickTrackInfo=pvid%3A1fe404fe-28ff-4cb5-b1d6-ee635c27bce1%3Bchannel_id%3A0000%3Bmt%3Ahot%3Bitem_id%3A329074822%3B",
+  },
+  {
+    id: 2,
+    name: "Product 3",
+    imageUrl: "https://ibb.co/Y3gtzY1",
+    affiliateLink:
+      "https://www.daraz.com.bd/products/30m-i329074822-s1702818159.html?spm=a2a0e.home.just4u.1.573712f70RJfqT&scm=1007.28811.376629.0&pvid=1fe404fe-28ff-4cb5-b1d6-ee635c27bce1&clickTrackInfo=pvid%3A1fe404fe-28ff-4cb5-b1d6-ee635c27bce1%3Bchannel_id%3A0000%3Bmt%3Ahot%3Bitem_id%3A329074822%3B",
+  },
+  {
+    id: 3,
+    name: "Product 4",
+    imageUrl: "https://ibb.co/Y3gtzY1",
+    affiliateLink:
+      "https://www.daraz.com.bd/products/30m-i329074822-s1702818159.html?spm=a2a0e.home.just4u.1.573712f70RJfqT&scm=1007.28811.376629.0&pvid=1fe404fe-28ff-4cb5-b1d6-ee635c27bce1&clickTrackInfo=pvid%3A1fe404fe-28ff-4cb5-b1d6-ee635c27bce1%3Bchannel_id%3A0000%3Bmt%3Ahot%3Bitem_id%3A329074822%3B",
+  },
+  {
+    id: 4,
+    name: "Product 5",
+    imageUrl: "https://ibb.co/Y3gtzY1",
+    affiliateLink:
+      "https://www.daraz.com.bd/products/30m-i329074822-s1702818159.html?spm=a2a0e.home.just4u.1.573712f70RJfqT&scm=1007.28811.376629.0&pvid=1fe404fe-28ff-4cb5-b1d6-ee635c27bce1&clickTrackInfo=pvid%3A1fe404fe-28ff-4cb5-b1d6-ee635c27bce1%3Bchannel_id%3A0000%3Bmt%3Ahot%3Bitem_id%3A329074822%3B",
+  },
+  {
+    id: 5,
+    name: "Product 6",
+    imageUrl: "https://ibb.co/Y3gtzY1",
+    affiliateLink:
+      "https://www.daraz.com.bd/products/30m-i329074822-s1702818159.html?spm=a2a0e.home.just4u.1.573712f70RJfqT&scm=1007.28811.376629.0&pvid=1fe404fe-28ff-4cb5-b1d6-ee635c27bce1&clickTrackInfo=pvid%3A1fe404fe-28ff-4cb5-b1d6-ee635c27bce1%3Bchannel_id%3A0000%3Bmt%3Ahot%3Bitem_id%3A329074822%3B",
+  },
+  {
+    id: 6,
+    name: "Product 7",
+    imageUrl: "https://ibb.co/XSzTvJ2",
+    affiliateLink: "https://example.com/affiliate-link-product-2",
+  },
+];
