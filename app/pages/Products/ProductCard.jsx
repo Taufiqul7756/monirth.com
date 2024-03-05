d53d8a39a5f510efd72dccb28048039c10251831
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-06 01:21:13 +0600

    responsive gap for all devices

--- app/pages/Products/ProductCard.jsx
+++ app/pages/Products/ProductCard.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const ProductCard = () => {
   console.log(affiliateProducts);
   return (
-    <div className="grid gap-y-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid gap-2 md:gap-6 lg:gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {affiliateProducts.map((product) => (
         <div
           key={product.id}
